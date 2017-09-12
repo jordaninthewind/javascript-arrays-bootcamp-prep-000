4abdae8005868d8eb640a3e73ff09fcf49f4cd54
jordaninthewind <jordan.kline@gmail.com>
2017-09-12 21:48:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
     return array[index];
 }
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop(element);
+  return array;
+}
