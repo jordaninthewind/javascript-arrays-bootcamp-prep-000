79c2f2921931118e8baabb711dc0b495f834d7e8
jordaninthewind <jordan.kline@gmail.com>
2017-09-12 20:53:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array = [element, ...array];
+  array = [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
