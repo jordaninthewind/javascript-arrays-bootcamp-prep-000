c7709deca291f2ccf9212120a32294fb69e927aa
jordaninthewind <jordan.kline@gmail.com>
2017-09-12 20:21:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var array = [...array, element];
-  return array;
+  var array2 = [...array, element];
+  return array2;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
