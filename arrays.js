883ab1728c61d761518ca4e9f945a5a3911eadf0
jordaninthewind <jordan.kline@gmail.com>
2017-09-12 21:58:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,7 +36,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  array = array.slice[array];
   return array;
 }
 
